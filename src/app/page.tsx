9d8348c4d194c8554a876c3b0ac442802fbad74e
Timothy Miamba <timal0361@gmail.com>
2025-11-26 09:53:52 +0200

    Add empty state to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
+import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { MessageSquareIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="container mx-auto max-2-3xl px-4 py-8 space-y-8"></div>
+    <div className="container mx-auto max-2-3xl px-4 py-8 space-y-8">
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant={"icon"}>
+            <MessageSquareIcon />
+          </EmptyMedia>
+          <EmptyTitle>No Chat Rooms</EmptyTitle>
+          <EmptyDescription>
+            Create a new chat room to get started
+          </EmptyDescription>
+          <EmptyContent>
+            <Button asChild>
+              <Link href={"rooms/new"}>Create Room</Link>
+            </Button>
+          </EmptyContent>
+        </EmptyHeader>
+      </Empty>
+    </div>
   );
 }
