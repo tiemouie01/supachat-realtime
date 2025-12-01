6902527723fe2850e63526d0086ae94e9608d29b
Timothy Miamba <timal0361@gmail.com>
2025-12-01 10:32:39 +0200

    Add leave room button

---
+++ src/components/leave-room-button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { ComponentProps } from "react";
+import { ActionButton } from "./ui/action-button";
+import { useCurrentUser } from "@/lib/services/supabase/hooks/useCurrentUser";
+import { createClient } from "@/lib/services/supabase/client";
+import { useRouter } from "next/navigation";
+
+export function LeaveRoomButton({
+  children,
+  roomId,
+  ...props
+}: Omit<ComponentProps<typeof ActionButton>, "action"> & {
+  roomId: string;
+}) {
+  const { user } = useCurrentUser();
+  const router = useRouter();
+
+  async function leaveRoom() {
+    if (user == null) {
+      return { error: true, message: "You must be logged in to leave a room" };
+    }
+
+    const supabase = createClient();
+    const { error } = await supabase
+      .from("chat_room_member")
+      .delete()
+      .eq("chat_room_id", roomId)
+      .eq("user_id", user.id);
+    if (error) {
+      return { error: true, message: "Failed to leave room" };
+    }
+    router.refresh();
+    router.push(`/rooms/${roomId}`);
+    return { error: false };
+  }
+  return (
+    <ActionButton {...props} action={leaveRoom}>
+      {children}
+    </ActionButton>
+  );
+}
