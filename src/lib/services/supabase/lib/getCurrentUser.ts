4dc0c1f3e93d7fba149a9c0db23d12c989b69c8f
Timothy Miamba <timal0361@gmail.com>
2025-12-02 09:57:23 +0200

    Cache user profiles

--- src/lib/services/supabase/lib/getCurrentUser.ts
+++ src/lib/services/supabase/lib/getCurrentUser.ts
@@ -1,6 +1,7 @@
+import { cache } from "react";
 import { createClient } from "../server";
 
-export async function getCurrentUser() {
+export const getCurrentUser = cache(async () => {
   const supabase = await createClient();
   return (await supabase.auth.getUser()).data.user;
-}
+});
