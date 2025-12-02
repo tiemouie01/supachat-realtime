c4fa49b7ddf9c36d62e59806c9cae32304ab4506
Timothy Miamba <timal0361@gmail.com>
2025-12-02 13:05:27 +0200

    Add supabase realtime functionality in React Hook

--- src/app/rooms/[id]/_client.tsx
+++ src/app/rooms/[id]/_client.tsx
@@ -3,6 +3,8 @@
 import { ChatInput } from "@/components/chat-input";
 import { ChatMessage } from "@/components/chat-message";
 import { Message } from "@/lib/services/supabase/actions/messages";
+import { createClient } from "@/lib/services/supabase/client";
+import { useEffect, useState } from "react";
 
 export function RoomClient({
   room,
@@ -20,14 +22,17 @@ export function RoomClient({
   };
   messages: Message[];
 }) {
+  const { connectedUsers } = useRealtimeChat({
+    roomId: room.id,
+    userId: user.id,
+  });
   return (
     <div className="container mx-auto h-screen-with-header border border-y-0 flex flex-col">
       <div className="flex items-center justify-between gap-2">
         <div className="p-4 border-b">
           <h1 className="text-2xl font-bold">{room.name}</h1>
           <p>
-            0 users online
-            {/* {connectedUsers.length} connected users */}
+            {connectedUsers} user{connectedUsers > 1 ? "s" : ""} online
           </p>
           {/* <InviteUserModal roomId={room.id} /> */}
         </div>
@@ -56,4 +61,44 @@ function InviteUserModal({ roomId }: { roomId: string }) {
       <h1>Invite User</h1>
     </div>
   );
+}
+
+function useRealtimeChat({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) {
+  const [connectedUsers, setConnectedUsers] = useState<number>(1);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const supabase = createClient();
+    const newChannel = supabase.channel(`room:${roomId}:messages`, {
+      config: {
+        private: true,
+        presence: {
+          key: userId,
+        },
+      },
+    });
+
+    newChannel
+      .on("presence", { event: "sync" }, () => {
+        setConnectedUsers(Object.keys(newChannel.presenceState()).length);
+      })
+      .subscribe((status) => {
+        if (status !== "SUBSCRIBED") return;
+
+        newChannel.track({ userId });
+      });
+
+    return () => {
+      newChannel.untrack();
+      newChannel.unsubscribe();
+    };
+  }, [roomId, userId]);
+
+  return { connectedUsers, messages };
 }
